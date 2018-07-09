eeb846f86c6d89e2890c81491a70ca80a38ad94d
Linus Gasser <linus.gasser@epfl.ch>
2018-07-09 08:24:48 +0200

    wip

--- omniledger/service/proto.go
+++ omniledger/service/proto.go
@@ -14,6 +14,7 @@ import (
 // type :darc.ID:bytes
 // type :Arguments:[]Argument
 // type :Instructions:[]Instruction
+// type :Version:sint32
 // package omniledger;
 // import "skipchain.proto";
 // import "onet.proto";
